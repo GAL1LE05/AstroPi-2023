bddffee4435afa62ad9fb2dbfe3242c073540258
Bruno Duarte <bruno.rabacal.duarte@gmail.com>
2023-01-19 17:27:10 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+from picamera import PiCamera
+from time import sleep
+from orbit import ISS
+from pathlib import Path
+
+def convert(angle):
+    """
+    Convert a `skyfield` Angle to an EXIF-appropriate
+    representation (positive rationals)
+    e.g. 98° 34' 58.7 to "98/1,34/1,587/10"
+
+    Return a tuple containing a boolean and the converted angle,
+    with the boolean indicating if the angle is negative.
+    """
+    sign, degrees, minutes, seconds = angle.signed_dms()
+    exif_angle = f'{degrees:.0f}/1,{minutes:.0f}/1,{seconds*10:.0f}/10'
+    return sign < 0, exif_angle
+
+def capture(camera, image):
+    """Use `camera` to capture an `image` file with lat/long EXIF data."""
+    point = ISS.coordinates()
+
+    # Convert the latitude and longitude to EXIF-appropriate representations
+    south, exif_latitude = convert(point.latitude)
+    west, exif_longitude = convert(point.longitude)
+
+    # Set the EXIF tags specifying the current location
+    camera.exif_tags['GPS.GPSLatitude'] = exif_latitude
+    camera.exif_tags['GPS.GPSLatitudeRef'] = "S" if south else "N"
+    camera.exif_tags['GPS.GPSLongitude'] = exif_longitude
+    camera.exif_tags['GPS.GPSLongitudeRef'] = "W" if west else "E"
+
+    # Capture the image
+    camera.capture(image)
+
+
+camera = PiCamera()
+camera.resolution = (2591, 1944)
+base_folder = Path(__file__).parent.resolve()
+
+
+for i in range(3*60):
+    capture(camera, f'image._{i:03d}.jpg') #outputs image with filename image_xxx.jpg
+    sleep(60)
