22f6fa3bfdd2f92b637e268a243dcc7ed86f2d39
Bruno Duarte <bruno.rabacal.duarte@gmail.com>
2023-01-26 22:14:01 +0000

    attempt at a different approach to ndvi calculation and conversion

--- ndvi.py
+++ ndvi.py
@@ -2,14 +2,14 @@ import numpy as np
 from PIL import Image
 
 # Open the image
-img = Image.open("1st Test Run\image._000.jpg")
+img = Image.open("test_image98.jpeg")
 
 # Convert the image to a numpy array
 img_data = np.array(img)
 
 # Extract the red and NIR channels
 red = img_data[:,:,0]
-nir = img_data[:,:,1]
+nir = img_data[:,:,2]
 
 # Calculate NDVI
 ndvi = (nir - red) / (nir + red)
