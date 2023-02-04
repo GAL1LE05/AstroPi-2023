21ccc0ec10215f42268a25ca6930710e69570748
GAL1LE05 <bruno.rabacal.duarte@gmail.com>
2023-02-04 19:55:31 +0000

    ;-;

--- ndvi.py
+++ ndvi.py
@@ -11,16 +11,17 @@ scalefactor = 2
 
 
 def display(image, image_name, delay=5, scalefactor=2):
-    """convert the image into an array of the RGB values that make up each pixel"""
+    """
+    This function takes an image, image name (string) as arguments, as well as a delay in seconds and a scale factor, both optional and defaulting to 5 and 2 respectively.
+    It scales the image provided by the inverse of the scale factor and shows it for the amount of time specified by the delay or, optionally if the delay is specified to be -1, it shows the image until input by the user through a keyboard.
+    Upon the end of the specified amount of time or user input, all windows are closed and the function ends, returning nothing.
+    """
     image = np.array(image, dtype=float)/float(255)
 
-    """scale the image by the factor specified (2 by default)"""
     shape = image.shape
     height = int(shape[0]/scalefactor)
     width = int(shape[1]/scalefactor)
     image = cv2.resize(image, (width, height))
-
-    """display the image for the time period of the delay (5 seconds by default) and then close"""
     cv2.namedWindow(image_name)
     cv2.imshow(image_name, image)
     if delay != -1:
@@ -31,6 +32,11 @@ def display(image, image_name, delay=5, scalefactor=2):
 
 
 def contrast_stretch(im):
+    """
+    This function takes in an image as argument and, taking into account the min and max values of the pixels, it stretches the contrast of the image as to make small changes more evident.
+    Upon completetion, it returns the processed image.
+    If the image is a single colour, then the operation will throw a division by zero exception, since both the minimum and maximum values of the pixels are the same. This is taken into account and a try, except clause was added for that case.
+    """
     in_min = np.percentile(im, 5)
     in_max = np.percentile(im, 95)
 
@@ -47,12 +53,22 @@ def contrast_stretch(im):
 
 
 def calc_ndvi(image):
+    """
+    This function takes an image as argument.
+    The image is processed according to NDVI calculation model, based on the reflectance of red and infrared radiation by healthy vegetation, returning a greyscale image representing the NDVI values from lowest (darkest) to highest (lightest).
+    """
     b, g, r = cv2.split(image)
     bottom = (r.astype(float) + b.astype(float))
     bottom[bottom == 0] = 0.01
     ndvi = (b.astype(float) - r)/bottom
     return ndvi
 
+"""
+Looping through every file and folder in the current directory (without recursiveness),
+for every file with extension '.jpg' or '.jpeg', it'll be processed into a greyscale NDVi image,
+which is saved in the same directory with a filename of the form {filename}_ndvi.png, where filename is the name of the original image.
+This is done continuously and without user input, with the elapsed time being calculated and printed to the console upon the termination of the loop.
+"""
 
 start_time = datetime.now()
 
@@ -70,7 +86,8 @@ for image in os.listdir(base_folder):
         # display(ndvi, 'NDVI', -1)
         ndvi_contrasted = contrast_stretch(ndvi)
         # display(ndvi_contrasted, 'NDVI Contrasted', -1)
-        cv2.imwrite(str(base_folder / str(filename + '_ndvi.png')), ndvi_contrasted)
+        cv2.imwrite(str(base_folder / str(filename + '_ndvi.png')),
+                    ndvi_contrasted)
 
 finish_time = datetime.now()
 elapsed = finish_time - start_time
